87abe62ba4fb2152cb78b70f76eba40b1376d96f
sl4shbored <39769731+sl4shbored@users.noreply.github.com>
2019-01-11 16:52:44 +0100

    changed something

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> I smiled that :-) 
+            Edit <code>src/App.js</code> I smiled that :-) hahahahahahahahahahahahahaahahah
           </p>
           <a
             className="App-link"
