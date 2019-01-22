a775ecec161fe3badca15bbabfc31c91c8146c27
sl4shbored <39769731+sl4shbored@users.noreply.github.com>
2019-01-22 14:19:48 +0100

    redid something

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             YES WE CAN 
+            reproduce the ol ddevoops thingy...
           </p>
           <a
             className="App-link"
